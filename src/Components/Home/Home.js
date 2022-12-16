94af6079bf8fdf68101ae655dfba0239d15a23c7
AJ <jonnaajay999@gmail.com>
2022-12-16 12:17:17 +0530

    verifying email

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Contex from "../Store/Contex";
 import "./Home.css";
 
 const Home = () => {
+  const contexVal = useContext(Contex);
   const navigator = useNavigate();
+
   const gotoProfile = () => {
     navigator("/profile");
+  };
+  const verifyEmail = async () => {
+    try {
+      const obj = {
+        requestType: "VERIFY_EMAIL",
+        idToken: localStorage.getItem("token"),
+      };
+
+      const res = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCCO6oxBDXyShDKpQc3CuIvIiZCPNoSXQA",
+        obj
+      );
+      console.log(res);
+    } catch (err) {
+      alert("somthing went wrong");
+    }
   };
   return (
     <>
@@ -14,6 +34,10 @@ const Home = () => {
 
         <button className="home" onClick={gotoProfile}>
           your profile is incomplete
+        </button>
+
+        <button className="home" onClick={verifyEmail}>
+          verify email
         </button>
       </div>
       <hr />
